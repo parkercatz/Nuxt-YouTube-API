20d270f91c0e20b155ecd094644a93f2b286382d
ParkerCatz <tksm.webdesign09@gmail.com>
2021-04-23 22:16:59 +0900

    feat: Videosコンポーネントの追加

---
+++ components/Videos.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex flex-column justify-start align-centar fluid">
+    <v-container>
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <v-row class="align-start justify-start">
+        <v-col
+          v-for="video in videos"
+          :key="video.snippet.resourceId.videoId"
+          cols="12"
+          md="4"
+        >
+          <iframe
+            :src="`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`"
+            frameborder="0"
+          ></iframe>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    videos,
+  },
+  data() {
+    return {
+      title: 'Videos',
+      description: 'My PlayList Videos',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Video />
+  <Video :videos="videos" />
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  components: {},
+  async asyncData() {
+    const playlistId = 'PLou3zjMo7ZedWTV_ZY8ZwbILgxbW-SSBE'
+    const part = 'snippet'
+    const baseUrl = `https://youtube.googleapis.com/youtube/v3/playlists?part=${part}&key=${playlistId}`
+    const results = await axios
+      .get(baseUrl)
+      .then((result) => {
+        return result.data.items
+      })
+      .catch(() => {})
+    return {
+      videos: results,
+    }
+  },
 }
 </script>
